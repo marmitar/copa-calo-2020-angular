79b6f04b7f5aed6c705f45d16f3b7e967c2a2fb6
tiagodepalves <tiagodepalves@gmail.com>
2020-02-27 00:43:59 -0300

    Experiment with Angular Fire

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core'
+import { Component, OnInit, EventEmitter } from '@angular/core'
+import { Observable } from 'rxjs'
+import { exhaustMap } from 'rxjs/operators'
+
+import { AngularFireAuth } from '@angular/fire/auth'
+import { AngularFireFunctions } from '@angular/fire/functions'
+
+import { User } from 'firebase/app'
+import 'firebase/firestore'
+
+
+interface AppUser extends User {
+    role?: string
+}
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-    title = 'copa-calo'
+export class AppComponent implements OnInit {
+    readonly title = 'copa-calo'
+    user: Observable<AppUser | null>
+    private generator: (({}) => Observable<string>)
+    text: EventEmitter<string>
+
+    constructor(
+        private auth: AngularFireAuth,
+        private functions: AngularFireFunctions
+    ) { }
+
+    ngOnInit() {
+        this.text = new EventEmitter(true)
+
+        this.user = this.auth.user.pipe(
+            exhaustMap(async user => {
+                if (user === null) {
+                    return Promise.resolve(null)
+                }
+
+                const result = await user.getIdTokenResult()
+                return Object.assign({}, user, result.claims)
+            })
+        )
+
+        this.generator = this.functions.httpsCallable('generatePassword')
+    }
+
+    async generate() {
+        const text = await this.generator({}).toPromise()
+
+        this.text.emit(text)
+    }
 }
