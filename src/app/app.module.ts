c27e8ed9e3304e9b4aacdf4769f43f0e45adc19e
tiagodepalves <tiagodepalves@gmail.com>
2020-02-27 00:43:41 -0300

    Anhular Fire modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { NgModule } from '@angular/core'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
+import { LoginComponent } from './login/login.component'
+import { environment } from '../environments/environment'
+
+import { ReactiveFormsModule } from '@angular/forms'
+import { AngularFireModule } from '@angular/fire'
+import { AngularFireAuthModule } from '@angular/fire/auth'
+import { AngularFirestoreModule } from '@angular/fire/firestore'
+import { AngularFireFunctionsModule } from '@angular/fire/functions'
+
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        LoginComponent
     ],
     imports: [
         BrowserModule,
-        AppRoutingModule
+        AppRoutingModule,
+        ReactiveFormsModule,
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFireAuthModule,
+        AngularFirestoreModule,
+        AngularFireFunctionsModule
     ],
-    providers: [],
+    providers: [environment.functions],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
