35035e8c3612094c0098e21938d99bf36726d161
tiagodepalves <tiagodepalves@gmail.com>
2020-03-01 16:21:41 -0300

    Login popup using `LoadingService`
    Advancing #17:
      - Done: #17 (comment 296537224)
      - Still missing: sign up and error handling

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -7,6 +7,8 @@ import { map } from 'rxjs/operators'
 import * as firebase from 'firebase'
 type UserCredental = firebase.auth.UserCredential
 
+import { LoadingService } from '$$/loading.service'
+
 
 @Component({
     selector: 'app-login',
@@ -38,6 +40,7 @@ export class LoginComponent {
     styleUrls: ['./login.component.scss']
 })
 export class LoginDialogComponent {
+
     readonly form = this.fb.group({
         email: [null, [Validators.required, Validators.email]],
         password: [null, Validators.required]
@@ -46,12 +49,34 @@ export class LoginDialogComponent {
     constructor(
         private dialog: MatDialogRef<LoginDialogComponent, UserCredental>,
         private fb: FormBuilder,
-        private auth: AngularFireAuth
+        private auth: AngularFireAuth,
+        private loading: LoadingService
     ) { }
 
     async login() {
-        const {email, password} = this.form.value
-        const creds = await this.auth.signInWithEmailAndPassword(email, password)
-        this.dialog.close(creds)
+        try {
+            const {email, password} = this.form.value
+            const signIn = this.auth.signInWithEmailAndPassword(email, password)
+            return await this.loading.runOn(signIn)
+
+        } catch (err) {
+            console.log(typeof err)
+            console.log(err)
+
+        } finally {
+            this.dialog.close()
+        }
     }
+
+    // private async wait<T>(x: T, error?: boolean): Promise<T> {
+    //     return new Promise((resolve, reject) => {
+    //         setTimeout(() => {
+    //             if (error) {
+    //                 reject(x)
+    //             } else {
+    //                 // resolve(x)
+    //             }
+    //         }, 1500)
+    //     })
+    // }
 }
