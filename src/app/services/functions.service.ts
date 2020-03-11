2d985eeaad4bee36526586572a9a3a6ab8530e4e
tiagodepalves <tiagodepalves@gmail.com>
2020-03-11 14:44:21 -0300

    Some general app observers
    - Observable for handset and portrait mode
    - user role and related observable taken from FunctionsService

--- src/app/services/functions.service.ts
+++ src/app/services/functions.service.ts
@@ -1,20 +1,9 @@
 import { Injectable } from '@angular/core'
 import { AngularFireFunctions } from '@angular/fire/functions'
-import { AngularFireAuth } from '@angular/fire/auth'
 
-import { Observable, OperatorFunction, of } from 'rxjs'
-import { exhaustMap, throwIfEmpty, first, filter, map } from 'rxjs/operators'
+import { Observable } from 'rxjs'
+import { ObserversService, Role } from '$$/observers.service'
 
-import { customClaims } from '@angular/fire/auth-guard'
-
-
-export type Role = {
-    role?: 'admin' | 'none' | 'arb'
-    team?: undefined
-} | {
-    role: 'dm',
-    team: string
-}
 export type User = Role & {email: string}
 
 
@@ -25,57 +14,21 @@ export class FunctionsService {
 
     constructor(
         private fns: AngularFireFunctions,
-        private auth: AngularFireAuth
+        private obs: ObserversService
     ) { }
-
-    userRole(): Observable<Role> {
-        return this.auth.user.pipe(
-            exhaustMap(user => {
-                if (user === null) {
-                    return of({})
-
-                } else {
-                    return customClaims(of(user))
-                }
-            }),
-            map(claims => claims ?? {})
-        )
-    }
-
-    isAdmin(): Observable<boolean> {
-        return this.userRole().pipe(
-            map(claims => claims.role === 'admin')
-        )
-    }
-
-    private requireRole<T>(role: Role): OperatorFunction<T, T> {
-        function match(claims?: any) {
-            return claims?.role === role.role
-                && (!role.team || claims?.team === role.team)
-        }
-
-        return obs => this.auth.user.pipe(
-            first(),
-            filter(user => user !== null),
-            customClaims,
-            filter(match),
-            throwIfEmpty(() => new Error('Usuário não tem permissão')),
-            exhaustMap(() => obs)
-        )
-    }
 
     generatePassword(): Observable<string> {
         const func = this.fns.httpsCallable('generatePassword')
-        return func(null).pipe(this.requireRole({role: 'admin'}))
+        return func(null).pipe(this.obs.requireRole({role: 'admin'}))
     }
 
     updateUser(email: string, password?: string, role?: Role): Observable<void> {
         const func = this.fns.httpsCallable('updateUser')
-        return func({email, password, role}).pipe(this.requireRole({role: 'admin'}))
+        return func({email, password, role}).pipe(this.obs.requireRole({role: 'admin'}))
     }
 
     listAllUsers(): Observable<User[]> {
         const func = this.fns.httpsCallable('listAllUsers')
-        return func(null).pipe(this.requireRole({role: 'admin'}))
+        return func(null).pipe(this.obs.requireRole({role: 'admin'}))
     }
 }
---
+++ src/app/services/observers.service.ts
@@ -0,0 +1,75 @@
+import { Injectable } from '@angular/core'
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout'
+import { AngularFireAuth } from '@angular/fire/auth/auth'
+
+import { of, OperatorFunction } from 'rxjs'
+import {
+    pluck, shareReplay, exhaustMap,
+    map, first, filter, throwIfEmpty
+} from 'rxjs/operators'
+
+
+export type Role = {
+    role?: 'admin' | 'none' | 'arb'
+    team?: undefined
+} | {
+    role: 'dm',
+    team: string
+}
+
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ObserversService {
+
+    constructor(
+        private bpObserver: BreakpointObserver,
+        private auth: AngularFireAuth
+    ) { }
+
+    readonly isHandset$ = this.bpObserver.observe(Breakpoints.Handset).pipe(
+        pluck('matches'),
+        shareReplay(1)
+    )
+
+    readonly isHPortrait$ = this.bpObserver.observe(Breakpoints.HandsetPortrait).pipe(
+        pluck('matches'),
+        shareReplay(1)
+    )
+
+    readonly user$ = this.auth.user.pipe(
+        exhaustMap(user => {
+            if (user !== null && user !== undefined) {
+                return user.getIdTokenResult().then<Role>(
+                    result => result.claims ?? {}
+                )
+            } else {
+                return of(null)
+            }
+        }),
+        shareReplay(1)
+    )
+
+    readonly isAdmin$ = this.hasRole({role: 'admin'})
+
+    hasRole(role: Role) {
+        function match(claims?: Role | null) {
+            return claims?.role === role.role
+                && (!role.team || claims?.team === role.team)
+        }
+
+        return this.user$.pipe(map(match))
+    }
+
+    requireRole<T>(role: Role): OperatorFunction<T, T> {
+        const msg = 'Usuário não tem permissão para isso'
+
+        return obs => this.hasRole(role).pipe(
+            first(),
+            filter(hasRole => hasRole),
+            throwIfEmpty(() => new Error(msg)),
+            exhaustMap(() => obs)
+        )
+    }
+}
